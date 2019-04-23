212002a2a0469eaea8c9655a9536d305d6bed313
ablaz <ablaz@10.151.222.198>
2019-04-23 10:26:52 +0200

    Junit

---
+++ src/es/upm/dit/isst/soa/ResultadosTest.java
@@ -0,0 +1,14 @@
+package es.upm.dit.isst.soa;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ResultadosTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
