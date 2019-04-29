9d01ad78bc8939e7017df28853ad79bda03804cb
ablaz <ablaz@Delle6540>
2019-04-29 22:37:16 +0200

    Tests DAO

---
+++ tests/es/upm/dit/isst/persistence/dao/Partido2016DAOImplementacionTest.java
@@ -0,0 +1,29 @@
+package es.upm.dit.isst.persistence.dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class Partido2016DAOImplementacionTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testCreate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testReadAll() {
+		fail("Not yet implemented");
+	}
+
+}
